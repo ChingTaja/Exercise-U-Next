abed41367241a12e0a52c0e8a52071b3b2b9b225
Taja <taja526526@gmail.com>
2025-07-03 22:31:29 +0800

    feat: route

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
-      </h1>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>Time to get started!</h1>
+
+      <Link href="/meals">share</Link>
+      <Link href="/meals/share">community</Link>
+      <Link href="/meals/slug1">slug</Link>
+      <Link href="/community">community</Link>
     </main>
   );
 }
