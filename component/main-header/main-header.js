708a5d8dbeed36e130ef03f77df9f5939f78d772
Taja <taja526526@gmail.com>
2025-07-06 10:09:35 +0800

    feat: header

---
+++ component/main-header/main-header.js
@@ -0,0 +1,29 @@
+import logoImg from '@/assets/logo.png';
+import Link from 'next/link';
+import classes from './main-header.module.css';
+import Image from 'next/image';
+import MainHeaderBackground from './main-header-background';
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="food" priority />
+          Nextlevel food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Broswer Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodie Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
