8ee59f08dbdc1fce274a7f9a37c33c87edbe1a5c
Taja <taja526526@gmail.com>
2025-07-10 23:05:23 +0800

    feat: store server action in separate file

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,6 +1,7 @@
 import classes from './page.module.css';
-import ImagePicker from '@/component/meals/image-picker';
-
+import ImagePicker from '@/component/meals/image-picker';`
+import { shareMeal } from '@/libs/actions';
+`
 export default function ShareMealPage() {
   return (
     <>
@@ -11,7 +12,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -34,7 +35,7 @@ export default function ShareMealPage() {
             <label htmlFor="instructions">Instructions</label>
             <textarea id="instructions" name="instructions" rows="10" required></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Your Image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
