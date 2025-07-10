ecd737b9b3fa3d2021e3eadc1a6b9d41c6fc2e99
Taja <taja526526@gmail.com>
2025-07-10 21:58:28 +0800

    feat: preview picture

--- component/meals/image-picker.js
+++ component/meals/image-picker.js
@@ -1,19 +1,54 @@
-
-'use client'
-import { useRef } from 'react';
+'use client';
+import { useRef, useState } from 'react';
 
 import classes from './image-picker.module.css';
+import Image from 'next/image';
 
 export default function ImagePicker({ label, name }) {
-  const imageInput = useRef()
+  const [pickedImage, setPickedImage] = useState();
+  const imageInput = useRef();
+
   function handlePickClick() {
-    imageInput.current.click()
+    imageInput.current.click();
   }
+
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      setPickedImage(null);
+
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    // 當檔案讀取完成後，將結果（圖片的資料 URL）設定到 state
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+    // 讀取檔案並轉換為資料 URL（適用於圖片顯示預覽）
+    fileReader.readAsDataURL(file);
+  }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
-        <input className={classes.input} type="file" id={name} accept="image/png, image/jpeg" name={name} ref={imageInput} />
+        <div className={classes.preview}>
+          {!pickedImage && <p>No Image picked yet</p>}
+          {pickedImage && <Image src={pickedImage} alt="The image selected by the user" fill />}
+        </div>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={imageInput}
+          onChange={handleImageChange}
+          required
+        />
         <button type="button" className={classes.button} onClick={handlePickClick}>
           Pick Image
         </button>
